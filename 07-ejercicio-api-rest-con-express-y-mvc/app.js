0de6a52cc6335b8cd8f24fcfe1425408ef0da312
Retr09412 <carlosnicola39@gmail.com>
2026-04-06 05:19:50 -0500

    Ejercicio terminado.

--- 07-ejercicio-api-rest-con-express-y-mvc/app.js
+++ 07-ejercicio-api-rest-con-express-y-mvc/app.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { jobsRouter } from './routes/jobs.js'
+import {jobsRouter}  from './routes/jobs.js'
+import { DEFAULTS } from './config.js'  
+import { corsMiddleware } from './middlewares/cors.js'
 
-const PORT = 3000
+const PORT = process.env.PORT ?? DEFAULTS.PORT
 const app = express()
 
+app.use(express.json());
+app.use(corsMiddleware());
 app.use('/jobs', jobsRouter)
 
 app.listen(PORT, () => {
