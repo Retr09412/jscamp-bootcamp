44ebd197add7baace06ae3334d65a04512ab44ee
Retr09412 <carlosnicola39@gmail.com>
2026-04-06 05:17:19 -0500

    Ejercicio terminado.
    Implement CORS middleware to handle accepted origins.

--- 07-ejercicio-api-rest-con-express-y-mvc/middlewares/cors.js
+++ 07-ejercicio-api-rest-con-express-y-mvc/middlewares/cors.js
@@ -1,3 +1,21 @@
-import cors from 'cors'
+import cors from 'cors';
 
-/* Aquí debe ir la lógica de tu middleware */
+const ACCEPTED_ORIGINS = [
+  'http://localhost:3000',
+  'http://localhost:1234',
+  'https://midu.dev',
+  'http://jscamp.dev',
+  'http://localhost:5173'
+];
+
+export const corsMiddleware = ({accepted_origins = ACCEPTED_ORIGINS} = {}) => {
+  return cors({
+    origin: (origin, callback) => {
+      if (accepted_origins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error('Origin not allowed'));
+    }
+  });
+}
+
