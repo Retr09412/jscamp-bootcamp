2f1e4448040875da81083c04e1cd160c24804906
Retr09412 <carlosnicola39@gmail.com>
2026-04-22 15:22:03 -0500

    Terminado

--- 08-ejercicio-testing-con-node-y-zod/schemas/jobs.js
+++ 08-ejercicio-testing-con-node-y-zod/schemas/jobs.js
@@ -1,10 +1,44 @@
-/*
- * Aquí debes crear el schema de validación con Zod para los jobs
- *
- * Recuerda:
- * - Importar zod
- * - Crear un schema que valide la estructura de un job
- * - Exportar funciones validateJob() y validatePartialJob()
- * - Usar safeParse() para validar sin lanzar excepciones
- * - Definir reglas de validación (min, max, required, optional, etc.)
- */
+import * as z from "zod";
+
+
+
+const jobSchema = z.object({
+  titulo: 
+    z.string(
+    {
+      required_error: "El titulo del trabajo es requerido", 
+      invalid_type_error: "El titulo debe ser un texto"
+    })
+    .min(3, {message: "EL titulo del trabajo debe tener al menos 3 caracteres"})
+    .max(100, {message: "El titulo del trabajo no puede tener mas de 100 caracteres"})
+  ,
+  empresa: 
+    z.string()
+    .min(1, {message: "La empresa es requerida"}),
+  ubicacion: 
+    z.string()
+    .min(1, {message: "La ubicación es requerida"}),
+  descripcion: 
+    z.string()
+    .optional(),
+  content: z.object({
+    description: z.string().optional(),
+    responsibilities: z.string().optional(),
+    requirements: z.string().optional(),
+    about: z.string().optional()
+  }).optional(),
+  data: z.object({
+    technology: z.array(z.string()).optional(),
+    modalidad: z.string().optional(),
+    nivel: z.string().optional(),
+  })
+    
+});
+
+export function validateJob(input) {
+  return jobSchema.safeParse(input);
+}
+
+export function validatePartialJob(input) {
+  return jobSchema.partial().safeParse(input);
+}
