2e95f9fe3000b822c0aea273a7f6f7d9d9f7a199
Retr09412 <carlosnicola39@gmail.com>
2026-04-22 15:19:46 -0500

    Terminado

--- 08-ejercicio-testing-con-node-y-zod/middlewares/cors.js
+++ 08-ejercicio-testing-con-node-y-zod/middlewares/cors.js
@@ -1,20 +1,30 @@
-import cors from 'cors'
+import cors from 'cors';
 
 const ACCEPTED_ORIGINS = [
-    'http://localhost:5173'
-]
+  'http://localhost:3000',
+  'http://localhost:1234',
+  'https://midu.dev',
+  'http://jscamp.dev',
+  'http://localhost:5173',
+  'http://localhost:3456'
+];
 
-export const corsMiddleware = ({acceptedOrigins = ACCEPTED_ORIGINS} = {}) => {
+export const corsMiddleware = ({accepted_origins = ACCEPTED_ORIGINS} = {}) => {
+  return cors({
+    origin: (origin, callback) => {
+      if (process.env.NODE_ENV === 'test') {
+        return callback(null, true);
+      }
 
-    return cors({
-        origin: (origin, callback) => {
+      if (accepted_origins.includes(origin)) {
+        return callback(null, true);
+      }
 
-            if (acceptedOrigins.includes(origin) || !origin){
-                return callback(null, true)
-            }
+      if (!origin) {
+        return callback(null, true);
+      }
 
-            return callback(new Error('Origen no permitido'))
-        }
-    })
+      return callback(new Error('Origin not allowed'));
+    }
+  });
 }
-
