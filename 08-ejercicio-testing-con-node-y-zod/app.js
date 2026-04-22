82ea27665c18a0fb2fb1fc68e49b1a2bd553b373
Retr09412 <carlosnicola39@gmail.com>
2026-04-22 15:22:40 -0500

    Terminado

--- 08-ejercicio-testing-con-node-y-zod/app.js
+++ 08-ejercicio-testing-con-node-y-zod/app.js
@@ -1,22 +1,19 @@
 import express from 'express'
+import {jobsRouter}  from './routes/jobs.js'
+import { DEFAULTS } from './config.js'  
 import { corsMiddleware } from './middlewares/cors.js'
-import { jobsRouter } from './routes/jobs.js'
-
-import { DEFAULTS } from './config.js'
 
 const PORT = process.env.PORT ?? DEFAULTS.PORT
-
 const app = express()
 
-app.use(corsMiddleware())
-app.use(express.json())
+export default app
 
+app.use(express.json());
+app.use(corsMiddleware());
 app.use('/jobs', jobsRouter)
 
-if (!process.env.NODE_ENV) {
+if(!process.env.NODE_ENV) {
   app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`)
+    console.log(`Servidor levantado en http://localhost:${PORT}`)
   })
 }
-
-export default app
